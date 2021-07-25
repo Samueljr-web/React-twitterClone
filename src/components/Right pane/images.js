8d1679f10db03d6f6f5e6a3a5ee33736491be860
Samueljr-web <70188223+Samueljr-web@users.noreply.github.com>
2021-07-25 12:04:42 +0100

    Add files via upload

---
+++ src/components/Right pane/images.js
@@ -0,0 +1,4 @@
+export const Engineering = "https://pbs.twimg.com/profile_images/1267117170858885120/Tmh6Jdmi_normal.jpg"
+export const Skysports = "https://pbs.twimg.com/profile_images/1346184104346267648/DMeDS04X_normal.jpg" 
+export const Chelsea = "https://pbs.twimg.com/profile_images/1410571506418081800/yPLO08Kf_400x400.jpg";
+
